94370f34f98b954080ff4922aad0e3f5d1052fc3
zeus012345 <francisjerry360@gmail.com>
2022-05-25 16:06:19 +0100

    initial commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,92 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+
+const NavBar = () => {
+
+    const mainMenu = [
+        {
+            id: 1,
+            name: 'Dashboard',
+            icon: 'assets/dashicon.svg',
+            link: '/'
+        },
+        {
+            id: 2,
+            name: 'Inbox',
+            icon: 'assets/chaticon.svg',
+            link: '/'
+        }
+    ];
+    const mainMenu2 = [
+        {
+            id: 1,
+            name: 'Accounts',
+            icon: 'assets/Usericon.svg',
+            link: '/'
+        },
+        {
+            id: 2,
+            name: 'Schedule Post',
+            icon: 'assets/Calendaricon.svg',
+            link: '/'
+        },
+        {
+            id: 3,
+            name: 'Communities',
+            icon: 'assets/Searchicon.svg',
+            link: '/'
+        },
+        {
+            id: 4,
+            name: 'Analytics',
+            icon: 'assets/Charticon.svg',
+            link: '/'
+        },
+        {
+            id: 5,
+            name: 'File & Folders',
+            icon: 'assets/Foldericon.svg',
+            link: '/'
+        },
+        {
+            id: 6,
+            name: 'Settings',
+            icon: 'assets/Setting_alt_line.svg',
+            link: '/'
+        },
+
+    ];
+
+    return (
+        <aside className="">
+            <nav className="w-full h-screen bg-[#0A194E] pt-7 pl-7">
+                <div className="logo  flex items-center justify-between ">
+                    <div className="flex gap-2 items-center mb-7 ">
+                        <img src="assets/logo.svg" alt="logo" />
+                        <h3 className="text-white">Trends</h3>
+                    </div>
+                    <div className="bg-[#51CBFF] rounded-[50%] p-3 flex relative left-5 -top-4 cursor-pointer text-[#0A194E] text-[1.8rem]">
+                        <i className="fa-solid fa-bars-staggered"></i>
+                    </div>
+                </div>
+                <h3>Main Menu</h3>
+                {mainMenu.map((menu1) => {
+                    return <div key={menu1.id} className="text-[#CCD2E3] flex gap-3 items-center pt-7">
+                        <img src={menu1.icon} alt="" />
+                        <NavLink className="hover:text-[#51CBFF] transition-all duration-400" to={menu1.link}>{menu1.name}</NavLink>
+                    </div>
+                })}
+                <h3 className="pt-7">Workspace</h3>
+                {mainMenu2.map((menu2) => {
+                    return <div key={menu2.id} className="text-[#CCD2E3] flex gap-3 items-center pt-7">
+                        <img src={menu2.icon} alt="" />
+                        <NavLink className="hover:text-[#51CBFF] transition-all duration-700" to={menu2.link}>{menu2.name}</NavLink>
+                    </div>
+                })}
+            </nav>
+
+        </aside>
+    )
+}
+
+export default NavBar
---
+++ src/components/TopDash.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TopDash = () => {
+    return (
+        <section className="">
+            <div className="">
+                <h2>Dashboard</h2>
+            </div>
+        </section>
+    )
+}
+
+export default TopDash
