e90ca722cae2baf349ac9e6f454f8b48829a448a
zeus012345 <francisjerry360@gmail.com>
2022-05-31 10:57:36 +0100

    mater

--- src/components/main/TopNav.jsx
+++ src/components/main/TopNav.jsx
@@ -4,9 +4,9 @@ import React from 'react'
 /* top NavBar section that houses the avatar, theme pallette and so on*/
 const TopNav = () => {
     return (
-        <div className=" flex  h-fit border items-center pl-10 pr-10 pt-7 pb-3 text-[#1C1F37] shadow-md w-full justify-between fixed top-0 z-50 bg-white">
-            <h2 className="xl:ml-[20%]  lg:ml-[25%] hidden sm:block">Dashboard</h2>
-            <div className="flex items-center text-[2.5rem] gap-7 transition-all duration-600 cursor-pointer">
+        <div className=" flex  h-fit border items-center pl-10 pr-10 pt-7 pb-3 text-[#1C1F37] shadow-md w-full justify-end sm:justify-between fixed top-0 z-50 bg-white">
+            <h2 className="xl:ml-[20%]  lg:ml-[25%] hidden sm:block sm:ml-20">Dashboard</h2>
+            <div className="flex items-center text-[2.5rem] gap-7 transition-all duration-600 cursor-pointer relative right-0">
                 <i className="fa-solid fa-magnifying-glass hover:text-[#51CBFF]"></i>
                 <div className="relative">
                     <i className="fa-regular fa-bell hover:text-[#51CBFF]"></i>
